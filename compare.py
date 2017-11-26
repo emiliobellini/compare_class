63bbd918f1448a34d567b55c15db3e57bd4a9f0f
Emilio Bellini <emilio.bellini@gmail.com>
2017-11-26 08:57:06 +0000

    Designed plan of the code

--- compare.py
+++ compare.py
@@ -15,4 +15,13 @@ CLASS_V2_DIR = BASE_DIR + '../hi_class_devel/'
 INPUT_DIR = BASE_DIR + 'init_files/'
 OUTPUT_DIR = BASE_DIR + 'output/'
 
+#List of task that this code has to do:
+#   1 - Import a file with the values/ranges of the parameters
+#   2 - Generate a single ini file
+#   3 - Run the two versions of class for the same ini file
+#   4 - Compare the outputs
+#   5 - Write a line in a table with the values of the parameters and the relative differences
+#   6 - If the agreement is not good keep the ini file
+
+
 #subprocess.call([CLASS_V1_DIR + 'class', INPUT_DIR + 'hi_class.ini'])
